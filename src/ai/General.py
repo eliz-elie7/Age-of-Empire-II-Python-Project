6557b17b37105bf765d5c019701bba965d8ed67b
Oumar68 <eloumarwann@gmail.com>
2025-11-07 18:19:36 +0100

    Generaux ajoutés

---
+++ src/ai/General.py
@@ -0,0 +1,19 @@
+from abc import ABC,abstractmethod
+
+
+class General(ABC):
+    "Classe pour tout les généraux AI"
+    def __init__(self,name:str):
+        self.name = name;
+        
+    "Methodes partagées"
+    def give_orders(self,current_player,all_players,map):
+        """current player = armée controlée
+        all players = tout le monde pour connaitre les enmmies
+        map = carte pour les déplacements"""
+        orders = []
+        return orders 
+        
+
+    def get_name(self):
+        return self.name;       
